324d75644295bf074ff03b37422fca4580179a56
tom.chen <tom603172002@gmail.com>
2017-09-22 11:24:02 +0800

    create home ap

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from weather.views import weather
+from home.views import home
 
 urlpatterns = [
+    url('', home),
     url(r'^admin/', admin.site.urls),
     url(r'^weather/$', weather),
 ]
