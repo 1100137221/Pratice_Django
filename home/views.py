44f2c4b9bee289d2d616eda0e13ef1116952affb
tom.chen <tom603172002@gmail.com>
2017-10-03 09:20:38 +0800

    更新 layout 位置

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from home.models import Music
 # Create your views here.
 
 
 def home(request):
+
+    print(Music.objects.get(pk=3))
+    print(Music.objects.all())
     return render(request, 'home.html')
