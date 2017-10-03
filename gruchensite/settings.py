dca2fa6a655178b97c05e451dcb8d410179a0031
tom.chen <tom603172002@gmail.com>
2017-10-03 11:58:49 +0800

    modify setting file

--- gruchensite/settings.py
+++ gruchensite/settings.py
@@ -121,6 +121,9 @@ ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
